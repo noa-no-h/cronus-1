58a83848394b9f48d3defefa5e2635a4f322d941
moritzWa <wallawitsch@gmail.com>
2025-06-14 18:46:06 -0700

    polish: settings

--- electron-app/src/renderer/src/components/settings/PermissionsStatus.tsx
+++ electron-app/src/renderer/src/components/settings/PermissionsStatus.tsx
@@ -195,10 +195,7 @@ export function PermissionsStatus() {
     return (
       <Card>
         <CardHeader>
-          <CardTitle className="flex items-center">
-            <Shield className="w-5 h-5 mr-2" />
-            Permissions
-          </CardTitle>
+          <CardTitle className="flex items-center">Permissions</CardTitle>
         </CardHeader>
         <CardContent>
           <div className="flex items-center justify-center py-8">
