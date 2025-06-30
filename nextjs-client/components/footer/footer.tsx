7a4c159b5ad7ddc56582bc8825d49863d3321e84
Cuvii <i@cuvii.dev>
2025-06-30 12:04:41 +0800

    feat: enhance Footer component with social media links and branding
    - Updated Footer component to include social media icons for GitHub, YouTube, Twitter, and Discord, improving user engagement.
    - Added a branding tagline and adjusted layout for better visual appeal and organization.
    - Ensured consistent styling with existing design patterns.

--- nextjs-client/components/footer/footer.tsx
+++ nextjs-client/components/footer/footer.tsx
@@ -1,19 +1,46 @@
 import Link from 'next/link';
 import type { ComponentProps } from 'react';
 import { cn } from '~/lib/cn';
+import { Discord } from '../icons/discord';
+import { GitHub } from '../icons/github';
+import { XformerlyTwitter } from '../icons/x';
+import { YouTube } from '../icons/youtube';
+import { TextLogo } from '../logo';
 
 export function Footer({ className, ...props }: ComponentProps<'footer'>) {
   return (
     <footer
       className={cn(
         'bg-[#f4f4f4] text-[#242437]',
-        'flex items-center gap-[100px]',
+        'flex justify-center items-center gap-[100px]',
         'pt-10 pb-20',
         className
       )}
       {...props}
     >
-      <div></div>
+      <div className="w-120">
+        <div>
+          <TextLogo className="w-[114px] text-[#36168D]" />
+        </div>
+        <p className="text-[#242437CC] text-sm mt-3">AI-Powered, Efficiency Revolution</p>
+
+        <div className="flex gap-6 mt-12 text-[#242437]">
+          <Link href={'#'}>
+            <GitHub className="size-5" />
+          </Link>
+          <Link href={'#'}>
+            <YouTube className="size-5" />
+          </Link>
+          <Link href={'#'}>
+            <XformerlyTwitter className="size-5" />
+          </Link>
+          <Link href={'#'}>
+            <Discord className="size-5" />
+          </Link>
+        </div>
+
+        <div className="mt-12">Cronus © {new Date().getFullYear()}</div>
+      </div>
 
       <div className="flex gap-[100px] text-[#242437CC] text-sm">
         <div className="space-y-[15px] flex flex-col">
