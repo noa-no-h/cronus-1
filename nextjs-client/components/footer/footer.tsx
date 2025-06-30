0a7df00be08bd074b8f789038284201f32da3595
Cuvii <i@cuvii.dev>
2025-06-30 11:49:42 +0800

    feat: enhance Footer component with additional resource links
    - Updated Footer component to include new sections for Resources and Company, improving navigation and user experience.
    - Organized links into distinct categories for better clarity and accessibility.

--- nextjs-client/components/footer/footer.tsx
+++ nextjs-client/components/footer/footer.tsx
@@ -15,11 +15,28 @@ export function Footer({ className, ...props }: ComponentProps<'footer'>) {
     >
       <div></div>
 
-      <div>
-        <div className="space-y-[15px] text-[#242437CC]">
+      <div className="flex gap-[100px] text-[#242437CC] text-sm">
+        <div className="space-y-[15px] flex flex-col">
           <h5 className="text-[#24243766] font-semibold leading-[28px]">PRODUCT</h5>
           <Link href={'/pricing'}>Pricing</Link>
           <Link href={'/docs'}>Docs</Link>
+        </div>
+
+        <div className="space-y-[15px] flex flex-col">
+          <h5 className="text-[#24243766] font-semibold leading-[28px]">RESOURCES</h5>
+          <Link href={'/audit-report'}>Audit Report</Link>
+          <Link href={'/changelog'}>Changelog</Link>
+          <Link href={'/blog'}>Blog</Link>
+          <Link href={'/faqs'}>FAQS</Link>
+          <Link href={'/press-kit'}>Press Kit</Link>
+        </div>
+
+        <div className="space-y-[15px] flex flex-col">
+          <h5 className="text-[#24243766] font-semibold leading-[28px]">COMPANY</h5>
+          <Link href={'/about'}>About us</Link>
+          <Link href={'/privacy'}>Privacy Policy</Link>
+          <Link href={'/contact'}>Contact us</Link>
+          <Link href={'/blog'}>Blog</Link>
         </div>
       </div>
     </footer>
