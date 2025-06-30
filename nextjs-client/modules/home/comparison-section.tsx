7dc01d3d2b8e28097f3527b7758d5fb5129255ac
Cuvii <i@cuvii.dev>
2025-06-30 11:37:04 +0800

    refactor: update font and layout in globals.css and layout.tsx, enhance ComparisonSection styling
    - Replaced Geist font with Inter in globals.css and layout.tsx for improved typography.
    - Updated ComparisonSection layout and styling for better visual presentation and clarity.
    - Adjusted section background and text colors for a more cohesive design.

--- nextjs-client/modules/home/comparison-section.tsx
+++ nextjs-client/modules/home/comparison-section.tsx
@@ -1,14 +1,65 @@
 import type { ComponentProps } from 'react';
+import { TextLogo } from '~/components/logo';
 import { cn } from '~/lib/cn';
 
 export function ComparisonSection({ className, ...props }: ComponentProps<'section'>) {
   return (
-    <section className={cn('bg-[#f4f4f4]', className)} {...props}>
-      <h3>Cronus vs others</h3>
+    <section
+      className={cn('bg-white text-[#242437] pt-40', 'flex flex-col items-center', className)}
+      {...props}
+    >
+      <h3 className="font-semibold text-[32px] tracking-[-3%] text-center">Cronus vs others</h3>
 
-      <div className="rounded-2xl border-[0.5px] border-[#dfdfdf]">
-        <div className="w-[464px] bg-[#faf8ff]"></div>
-        <div></div>
+      <div
+        className={cn(
+          'mt-20',
+          'inline-flex items-center',
+          'rounded-2xl overflow-hidden',
+          'border-[0.5px] border-[#dfdfdf] tracking-[-2%]',
+          'bg-[#fafafa]'
+        )}
+      >
+        <div
+          className={cn(
+            'w-[464px] bg-[#faf8ff] py-9 px-[56px] text-[#36168D]',
+            'border-r-[0.5px] border-[#DFDFDF]'
+          )}
+        >
+          <h5>
+            <TextLogo className="w-[108px]" />
+          </h5>
+          <ul
+            className={cn(
+              'text-xl',
+              'mt-5',
+              '[&>li]:py-5 [&>li]:border-t-[0.5px] [&>li]:border-[#36168D14]',
+              '[&>li:last-child]:pb-0'
+            )}
+          >
+            <li>Goal-aware distraction nudging</li>
+            <li>Passive + contextual</li>
+            <li>Cross-device analysis</li>
+            <li>Clean, proactive design</li>
+          </ul>
+        </div>
+        <div className="w-[464px] py-9 px-[56px]">
+          <h5 className="font-semibold text-2xl tracking-[-3%]  leading-[32px]">
+            Traditional Time Trackers
+          </h5>
+          <ul
+            className={cn(
+              'text-xl',
+              'mt-5',
+              '[&>li]:py-5 [&>li]:border-t-[0.5px] [&>li]:border-[#36168D14]',
+              '[&>li:last-child]:pb-0'
+            )}
+          >
+            <li>Rule-based blocking</li>
+            <li>Manual task tracking</li>
+            <li>Per-device stats only</li>
+            <li>Overwhelming, rigid, clunky</li>
+          </ul>
+        </div>
       </div>
     </section>
   );
