d3c2d2a5451472eaa00a3ca007060c93f1e1574e
Cuvii <i@cuvii.dev>
2025-07-01 14:22:54 +0800

    refactor: enhance responsiveness and layout in FeaturesSection with updated classNames and padding adjustments

--- nextjs-client/modules/home/features-section/index.tsx
+++ nextjs-client/modules/home/features-section/index.tsx
@@ -6,27 +6,30 @@ import mobileTrackingPng from './mobile-tracking.png';
 import scrollPreventionPng from './scroll-prevention.png';
 import smartNudgesPng from './smart-nudges.png';
 
+
+const cardClassnames = cn(
+  'pt-10 tablet:pt-16 desktop:pt-[54px]',
+  'bg-[#FAFAFA] rounded-2xl overflow-hidden',
+  'border-[0.5px] border-[#dfdfdf]'
+)
+
 export function FeaturesSection({ className, ...props }: ComponentProps<'section'>) {
   return (
     <section
-      className={cn('bg-white text-[#242437] py-[160px]', 'flex flex-col items-center', className)}
+      className={cn('bg-white text-[#242437]', 'flex flex-col items-center', 'py-16 tablet:py-[160px]','px-4', className)}
       {...props}
     >
       <h3 className="font-semibold text-[32px] tracking-[-3%] text-center">Other features</h3>
 
-      <ul className={cn("mt-20", "grid", "desktop:gap-6 desktop:grid-cols-[repeat(2,528px)]", " tablet:grid-cols-1 tablet:px-16 tablet:gap-6")}>
+      <ul className={cn("mt-12 tablet:mt-20", "grid gap-6","tablet:grid-cols-1 tablet:px-16", "desktop:grid-cols-[repeat(2,528px)]",)}>
         <li
-          className={cn(
-            'desktop:pt-[54px] tablet:pt-16',
-            'bg-[#FAFAFA] rounded-2xl overflow-hidden',
-            'border-[0.5px] border-[#dfdfdf]'
-          )}
+          className={cardClassnames}
         >
-          <div className="mx-8">
-            <h4 className="tracking-[-3%] font-semibold text-2xl">
+          <div className="px-6 tablet:px-8">
+            <h4 className="tracking-[-3%] font-semibold text-xl tablet:text-2xl">
               Calendar AI or manual tracking
             </h4>
-            <p className="tracking-[-2%] text-sm mt-6 leading-[1.5]">
+            <p className="tracking-[-2%] text-sm mt-4 tablet:mt-6 leading-[1.5]">
               Went to a conference, had a dinner with friends? Cronus helps you track your
               non-computer time using your calendar for context. Or create manual entries in one
               click.
@@ -36,15 +39,11 @@ export function FeaturesSection({ className, ...props }: ComponentProps<'section
         </li>
 
         <li
-          className={cn(
-            'pt-[54px]',
-            'bg-[#FAFAFA] rounded-2xl overflow-hidden',
-            'border-[0.5px] border-[#dfdfdf]'
-          )}
+          className={cardClassnames}
         >
-          <div className="mx-8">
-            <h4 className="tracking-[-3%] font-semibold text-2xl">Smart nudges</h4>
-            <p className="tracking-[-2%] text-sm mt-6 leading-[1.5]">
+          <div className="px-6 tablet:px-8">
+            <h4 className="tracking-[-3%] font-semibold text-xl tablet:text-2xl">Smart nudges</h4>
+            <p className="tracking-[-2%] text-sm mt-4 tablet:mt-6 leading-[1.5]">
               Cronus understands your distraction and productivity patterns and helps you avoid the
               root causes and helps you reach your daily and even life goals.
             </p>
@@ -53,15 +52,11 @@ export function FeaturesSection({ className, ...props }: ComponentProps<'section
         </li>
 
         <li
-          className={cn(
-            'pt-[54px]',
-            'bg-[#FAFAFA] rounded-2xl overflow-hidden',
-            'border-[0.5px] border-[#dfdfdf]'
-          )}
+          className={cardClassnames}
         >
-          <div className="mx-8">
-            <h4 className="tracking-[-3%] font-semibold text-2xl">Mobile tracking</h4>
-            <p className="tracking-[-2%] text-sm mt-6 leading-[1.5]">
+          <div className="px-6 tablet:px-8">
+            <h4 className="tracking-[-3%] font-semibold text-xl tablet:text-2xl">Mobile tracking</h4>
+            <p className="tracking-[-2%] text-sm mt-4 tablet:mt-6 leading-[1.5]">
               Cronus is the first cross-platform time and distraction tracking software. See how
               productive you were across all your devices in one dashboard (Comming Soon).
             </p>
@@ -70,15 +65,11 @@ export function FeaturesSection({ className, ...props }: ComponentProps<'section
         </li>
 
         <li
-          className={cn(
-            'pt-[54px]',
-            'bg-[#FAFAFA] rounded-2xl overflow-hidden',
-            'border-[0.5px] border-[#dfdfdf]'
-          )}
+          className={cardClassnames}
         >
-          <div className="mx-8">
-            <h4 className="tracking-[-3%] font-semibold text-2xl">Doom scroll prevention</h4>
-            <p className="tracking-[-2%] text-sm mt-6 leading-[1.5]">
+          <div className="px-6 tablet:px-8">
+            <h4 className="tracking-[-3%] font-semibold text-xl tablet:text-2xl">Doom scroll prevention</h4>
+            <p className="tracking-[-2%] text-sm mt-4 tablet:mt-6 leading-[1.5]">
               Cronus is not rule-based. Sometimes you need to go on LinkedIn or Twitter to look at
               people&apos;s profiles or just catch up with your network. However, doom scrolling is
               categorized as a distraction.
